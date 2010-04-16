23c8b1b40c883009dfeda0111f0edbfe87dbc07b
Tania Tudorache <tudorache@users.noreply.github.com>
2010-04-16 03:55:16 +0000

    refacotred

---
+++ src-util/edu/stanford/bmir/icd/bioportal/InsertLabelsFromTerms.java
@@ -0,0 +1,171 @@
+package edu.stanford.bmir.icd.bioportal;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.ncbo.stanford.bean.concept.ClassBean;
+import org.ncbo.stanford.util.BioportalConcept;
+
+import edu.stanford.smi.protege.exception.OntologyLoadException;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.jena.creator.OwlProjectFromUriCreator;
+import edu.stanford.smi.protegex.owl.model.OWLDatatypeProperty;
+import edu.stanford.smi.protegex.owl.model.OWLIndividual;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+
+public class InsertLabelsFromTerms {
+    private static Logger log = Log.getLogger(InsertLabelsFromTerms.class);
+   
+    public static final String BIOPORTAL_DELAY_PROPERTY="bioportal.call.delay";
+    public static final String ICD_ONTOLOGY_LOCATION_PROPERTY="labels.from.term.ontology.location";
+    public static final String LABEL_PROPERTY="labels.from.term.label.property";
+    public static final String REFERENCE_TERM_PROPERTY="labels.from.term.class";
+    public static final String RECURSIVE_PROPERTY="labels.from.term.recursive";
+    public static final String SNOMED_REST_URL="labels.from.term.rest.url.prefix";
+    public static final String TERM_ID_PROPERTY="labels.from.term.id.property";
+    
+
+    public static String ICD_NS = "http://who.int/icd#";
+
+    private String  labelPropertyName;
+    private boolean recursive;
+    private String  referenceTermName;
+    private String  restUrlPrefix;
+    private String  termIdPropertyName=ICD_NS + "termId";
+    
+    private Properties parameters = new Properties();
+    private OWLModel owlModel;
+    
+    public InsertLabelsFromTerms() throws FileNotFoundException, IOException {
+        parameters.load(new FileInputStream(new File("local.properties")));
+        labelPropertyName = parameters.getProperty(LABEL_PROPERTY);
+        referenceTermName = parameters.getProperty(REFERENCE_TERM_PROPERTY);
+        restUrlPrefix = parameters.getProperty(SNOMED_REST_URL);
+        recursive = parameters.getProperty(RECURSIVE_PROPERTY).toLowerCase().equals("true");
+        termIdPropertyName = parameters.getProperty(TERM_ID_PROPERTY);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void loadOntology() throws OntologyLoadException {
+        OwlProjectFromUriCreator creator = new OwlProjectFromUriCreator();
+        URI u = new File(parameters.getProperty(ICD_ONTOLOGY_LOCATION_PROPERTY)).toURI();
+        creator.setOntologyUri(u.toString());
+        List errors  = new ArrayList();
+        creator.create(errors);
+        if (!errors.isEmpty()) {
+            for (Object o : errors) {
+                if (o instanceof Throwable) {
+                    log.log(Level.WARNING, "Exception caught", (Throwable) o);
+                }
+                else {
+                    log.warning(o.toString());
+                }
+            }
+            throw new OntologyLoadException();
+        }
+        owlModel = creator.getOwlModel();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void saveOntology() throws IOException  {
+        List errors  = new ArrayList();
+        owlModel.getProject().save(errors);
+        if (!errors.isEmpty()) {
+            for (Object o : errors) {
+                if (o instanceof Throwable) {
+                    log.log(Level.WARNING, "Exception caught", (Throwable) o);
+                }
+                else {
+                    log.warning(o.toString());
+                }
+            }
+            throw new IOException();
+        }
+    }
+    
+    public void run() throws MalformedURLException {
+        OWLNamedClass referenceTermCls = owlModel.getOWLNamedClass(referenceTermName);
+        OWLDatatypeProperty termIdProperty = owlModel.getOWLDatatypeProperty(termIdPropertyName);
+        RDFProperty labelProperty = owlModel.getRDFProperty(labelPropertyName);
+        
+        int total = referenceTermCls.getInstanceCount(true);
+        int found = 0;
+        int counter = 0;
+        log.info("Examining " + total + " individuals");
+        for (Object o : referenceTermCls.getInstances(true)) {
+            if (++counter % 1000 == 0) {
+                log.info("" + counter + " individuals examined");
+            }
+            if (o instanceof OWLIndividual) {
+                OWLIndividual i = (OWLIndividual) o;
+                Object v = i.getPropertyValue(termIdProperty);
+
+                if (i.getPropertyValue(labelProperty) == null && 
+                        i.getPropertyValue(termIdProperty) != null &&
+                        v instanceof String) {
+                    String value = (String) v;
+                    
+                    String label = getLabelFromTermId(value);
+                    if (label != null) {
+                        if (++found % 100 == 0) {
+                            log.info("Added label property for " + found + " individuals");
+                        }
+                        i.setPropertyValue(labelProperty, label);
+                    }
+                }
+            }
+        }
+        log.info("Added Property level for " + found  + "individual");
+    }
+    
+    private String getLabelFromTermId(String termId) throws MalformedURLException {
+        try {
+            Long.parseLong(termId);
+        }
+        catch (NumberFormatException nfe) {
+            return null;
+        }
+        
+        ClassBean cb;
+        try {
+            cb = new BioportalConcept().getConceptProperties(new URL(restUrlPrefix + termId));
+        }
+        catch (Throwable t) {
+            return null;
+        }
+        if (cb != null) {
+            return cb.getLabel();
+        }
+        else {
+            return null;
+        }
+    }
+    
+    
+    /**
+     * @param args
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     * @throws OntologyLoadException 
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException, OntologyLoadException {
+        Logger.getLogger(BioportalConcept.class.getName()).setLevel(Level.SEVERE);
+        InsertLabelsFromTerms i = new InsertLabelsFromTerms();
+        i.loadOntology();
+        i.run();
+        i.saveOntology();
+    }
+
+}
---
+++ src-util/edu/stanford/bmir/icd/utils/Other_ICD_stuff.java
@@ -0,0 +1,208 @@
+package edu.stanford.bmir.icd.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import edu.stanford.bmir.icd.claml.ICDContentModel;
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.util.CollectionUtilities;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
+
+public class Other_ICD_stuff {
+
+    private static OWLModel owlModel;
+    private static ICDContentModel icdContentModel;
+    private static RDFProperty termIdProp;
+    private static RDFProperty defPrefilledInveProp;
+
+    public static void main(String[] args) {
+        Project prj = Project.loadProjectFromFile("/work/protege/projects/icd/content_model/icd_int/icd_mysql/icd_umbrella.pprj", new ArrayList());
+        owlModel = (OWLModel) prj.getKnowledgeBase();
+        icdContentModel = new ICDContentModel(owlModel);
+        termIdProp = owlModel.getRDFProperty("http://who.int/icd#termId");
+        defPrefilledInveProp = owlModel.getRDFProperty("http://who.int/icd/umls_definitions#is_prefilled_definition_of");
+
+        //printInvalidClamlRefs();
+        //fixClamlRef();
+        //printInvalidDefs();
+        //findInvalidDefs();
+        //fixInvalidDefs();
+
+        testCreateICDClass();
+    }
+
+
+    private static void testCreateICDClass() {
+        Collection<String> superClses = CollectionUtilities.createCollection("http://who.int/icd#II");
+        superClses.add("http://who.int/icd#V01-X59");
+
+        RDFSNamedClass cls = icdContentModel.createICDCategory(null, superClses);
+
+        System.out.println("Name: " + cls.getName() + " browser text: " + cls.getBrowserText());
+        System.out.println("Types: " + cls.getDirectTypes());
+
+        Collection<Slot> slots = cls.getOwnSlots();
+        for (Slot slot : slots) {
+            System.out.println(slot.getBrowserText() + ": " + cls.getOwnSlotValues(slot));
+        }
+    }
+
+    private static void printInvalidClamlRefs() {
+        long t0 = System.currentTimeMillis();
+        int i = 0;
+        Collection<RDFSNamedClass> icdCategories = icdContentModel.getICDCategories();
+        for (RDFSNamedClass category : icdCategories) {
+            if (i % 1000 == 0) {
+                System.out.println("Done " + i + " categories");
+            }
+            printInvalidTerm(category, category.getPropertyValues(icdContentModel.getInclusionProperty()));
+            printInvalidTerm(category, category.getPropertyValues(icdContentModel.getExclusionProperty()));
+            i++;
+        }
+
+        System.out.println("Time: " + ((System.currentTimeMillis() - t0)/1000) + " sec");
+    }
+
+    private static void printInvalidTerm(RDFSNamedClass cat, Collection<RDFResource> terms) {
+        for (RDFResource term : terms) {
+            Collection<RDFResource> clamlRefs = term.getPropertyValues(icdContentModel.getClamlReferencesProperty());
+            if (clamlRefs != null) {
+                for (Object element : clamlRefs) {
+                    Object clamlRef = element;
+                    if (clamlRef instanceof String) {
+                        System.out.println("Invalid for " + cat.getBrowserText() + ": " + clamlRef);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void fixClamlRef() {
+        int count = 0;
+        int fixedCount = 0;
+
+        RDFSNamedClass clamlRefClass = owlModel.getOWLNamedClass("http://who.int/icd#ClamlReference");
+        RDFProperty textProp = owlModel.getRDFProperty("http://who.int/icd#text");
+        RDFProperty icdRefProp = owlModel.getRDFProperty("http://who.int/icd#icdRefCode");
+
+        Collection<RDFResource> clamlRefs = clamlRefClass.getInstances(true);
+        for (RDFResource clamlTerm : clamlRefs) {
+            String text = (String) clamlTerm.getPropertyValue(textProp);
+            RDFResource ref = (RDFResource) clamlTerm.getPropertyValue(icdRefProp);
+            if (text != null && ref == null) {
+                Log.getLogger().warning("Missing icd ref: " + clamlTerm.getLocalName() + " text: " + text);
+                count++;
+                ref = owlModel.getRDFResource(text);
+                if (ref != null) {
+                    clamlTerm.setPropertyValue(icdRefProp, ref);
+                    fixedCount ++;
+                    Log.getLogger().info("** Fixed " + text + " -> " + ref.getBrowserText());
+
+                }
+            }
+        }
+
+        Log.getLogger().info("Found: " + count + " ; fixed: " + fixedCount);
+    }
+
+
+    private static void printInvalidDefs() {
+        long t0 = System.currentTimeMillis();
+        int i = 0;
+        int count = 0;
+        Collection<RDFSNamedClass> icdCategories = icdContentModel.getICDCategories();
+        for (RDFSNamedClass category : icdCategories) {
+            if (i % 1000 == 0) {
+                System.out.println("Done " + i + " categories");
+            }
+            count = count + printInvalidDef(category);
+            i++;
+        }
+
+        Log.getLogger().info("Time: " + ((System.currentTimeMillis() - t0)/1000) + " sec");
+        Log.getLogger().info("Found " + count + " invalid defs");
+
+    }
+
+
+    private static int printInvalidDef(RDFSNamedClass category) {
+        RDFResource defTerm = icdContentModel.getTerm(category, icdContentModel.getDefinitionProperty());
+        if (defTerm != null) {
+            Object defTermId = defTerm.getPropertyValue(termIdProp);
+            Collection isPrefilledOf = defTerm.getPropertyValues(defPrefilledInveProp);
+            if (isPrefilledOf != null && isPrefilledOf.size() > 1) {
+                Log.getLogger().warning("!!! Multiple is prefilled of " + category.getBrowserText() + " isPrefilledOf: " + isPrefilledOf);
+            }
+            if (defTermId != null) {
+                if (isPrefilledOf != null) {
+                    RDFResource inv = CollectionUtilities.getFirstItem(isPrefilledOf);
+                    if (!inv.equals(category)) {
+                        Log.getLogger().warning("**** Wrong inverses!!! " + category.getBrowserText() + " ? " + inv.getBrowserText());
+                    }
+                }
+                Log.getLogger().warning("Invalid def for " + category.getBrowserText() + " defTerm: " + defTerm.getLocalName() + " is Prefilled of: " + isPrefilledOf + "; termId:" + defTermId + " def: " + defTerm.getPropertyValue(icdContentModel.getLabelProperty()));
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private static void findInvalidDefs() {
+        int count = 0;
+
+        RDFSNamedClass defClass = owlModel.getOWLNamedClass("http://who.int/icd#DefinitionTerm");
+        RDFProperty ontIdProp = owlModel.getRDFProperty("http://who.int/icd#ontologyId");
+        RDFProperty invDefOfProp = owlModel.getRDFProperty("http://who.int/icd/umls_definitions#is_prefilled_definition_of");
+        RDFProperty termIdProp = owlModel.getRDFProperty("http://who.int/icd#termId");
+        RDFProperty defPrefilledProp = owlModel.getRDFProperty("http://who.int/icd#definitionPrefilled");
+        RDFProperty defProp = owlModel.getRDFProperty("http://who.int/icd#definition");
+
+        Collection<RDFResource> defTerms = defClass.getInstances(true);
+        for (RDFResource defTerm : defTerms) {
+            RDFResource invPrefilledDef = (RDFResource) defTerm.getPropertyValue(invDefOfProp); //categ
+            if (invPrefilledDef != null) {
+                if (!invPrefilledDef.hasPropertyValue(defPrefilledProp, defTerm)) {
+                    Log.getLogger().warning("Invalid def term for: " + invPrefilledDef.getBrowserText() + " term id: " + defTerm.getBrowserText());
+                    count++;
+                    if (!invPrefilledDef.hasPropertyValue(defProp, defTerm)) {
+                        Log.getLogger().warning("\t******* Check def for " + invPrefilledDef.getBrowserText());
+                    }
+                }
+            }
+        }
+        Log.getLogger().info("Invalid defs count: " + count);
+    }
+
+
+    private static void fixInvalidDefs() {
+        int count = 0;
+
+        RDFSNamedClass defClass = owlModel.getOWLNamedClass("http://who.int/icd#DefinitionTerm");
+        RDFProperty ontIdProp = owlModel.getRDFProperty("http://who.int/icd#ontologyId");
+        RDFProperty invDefOfProp = owlModel.getRDFProperty("http://who.int/icd/umls_definitions#is_prefilled_definition_of");
+        RDFProperty termIdProp = owlModel.getRDFProperty("http://who.int/icd#termId");
+        RDFProperty defPrefilledProp = owlModel.getRDFProperty("http://who.int/icd#definitionPrefilled");
+        RDFProperty defProp = owlModel.getRDFProperty("http://who.int/icd#definition");
+
+        Collection<RDFResource> defTerms = defClass.getInstances(true);
+        for (RDFResource defTerm : defTerms) {
+            RDFResource invPrefilledDef = (RDFResource) defTerm.getPropertyValue(invDefOfProp); //categ
+            if (invPrefilledDef != null) {
+                if (!invPrefilledDef.hasPropertyValue(defPrefilledProp, defTerm)) {
+                    invPrefilledDef.addPropertyValue(defPrefilledProp, defTerm);
+                    invPrefilledDef.removePropertyValue(defProp, defTerm);
+                    Log.getLogger().info("Fix def term for: " + invPrefilledDef.getBrowserText() + " term id: " + defTerm);
+                    count++;
+                }
+            }
+        }
+        Log.getLogger().info("Fixed defs count: " + count);
+    }
+
+}
