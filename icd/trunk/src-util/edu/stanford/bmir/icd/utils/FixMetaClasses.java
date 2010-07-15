53e6b6afb603d956fdb072b39e29b9fccfb021e0
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-07-15 01:04:09 +0000

    fix metaclasses script

---
+++ icd/trunk/src-util/edu/stanford/bmir/icd/utils/FixMetaClasses.java
@@ -0,0 +1,92 @@
+package edu.stanford.bmir.icd.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import edu.stanford.bmir.icd.claml.ICDContentModel;
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.ui.ProjectManager;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
+
+public class FixMetaClasses {
+    private static OWLModel owlModel;
+    private static ICDContentModel icdContentModel;
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Argument missing: pprj file name");
+            return;
+        }
+
+        Collection errors = new ArrayList();
+        Project prj = Project.loadProjectFromFile("/work/protege/projects/icd/content_model/icd_int/icd_mysql/icd_umbrella.pprj", errors);
+
+        if (errors != null) {
+            ProjectManager.getProjectManager().displayErrors("Errors", errors);
+        }
+
+        owlModel = (OWLModel) prj.getKnowledgeBase();
+
+        if (owlModel == null) {
+            System.out.println("Failed");
+            return;
+        }
+        icdContentModel = new ICDContentModel(owlModel);
+
+        fixMetaclasses();
+    }
+
+    private static void fixMetaclasses() {
+        long t0 = System.currentTimeMillis();
+
+        owlModel.setGenerateEventsEnabled(false);
+        RDFSNamedClass icdCatCls = icdContentModel.getICDCategoryClass();
+
+        RDFSNamedClass extCauseCls = icdContentModel.getExternalCausesTopClass();
+
+        Collection<RDFSNamedClass> catMetaclasses = icdContentModel.getRegularDiseaseMetaclasses();
+
+        Collection<RDFSNamedClass> topClses = icdCatCls.getDirectSubclasses();
+        topClses.remove(extCauseCls);
+
+        //fix regular disease
+        for (RDFSNamedClass topCls : topClses) {
+            fixMetaclses(topCls, catMetaclasses);
+            Collection<Cls> subclses = topCls.getSubclasses();
+            for (Cls subcls :subclses) {
+                if (subcls instanceof RDFSNamedClass) {
+                    fixMetaclses(subcls, catMetaclasses);
+                }
+            }
+        }
+
+        //fix external causes
+        Collection<RDFSNamedClass> extCauseMetaclses = icdContentModel.getExternalCauseMetaclasses();
+        Collection<RDFSNamedClass> extClses = extCauseCls.getSubclasses();
+        for (RDFSNamedClass cls : extClses) {
+            if (cls instanceof RDFSNamedClass) {
+                fixMetaclses(cls, extCauseMetaclses);
+            }
+        }
+
+        System.out.println("Time: " + (System.currentTimeMillis() - t0) /1000 + " sec");
+    }
+
+    private static void fixMetaclses(Cls c, Collection<RDFSNamedClass> metaclasses) {
+        for (RDFSNamedClass metacls : metaclasses) {
+            if (!c.hasType(metacls)) {
+                c.addDirectType(metacls);
+            }
+        }
+        Collection<Cls> extraMetaclases = new ArrayList<Cls>(c.getDirectTypes());
+        extraMetaclases.removeAll(metaclasses);
+
+        for (Cls metacls : extraMetaclases) {
+            c.removeDirectType(metacls);
+        }
+    }
+
+
+}
