63c667e89af7d4c33725e172e42b84e7b4b57bd8
nyulas <2441046+csnyulas@users.noreply.github.com>
2010-07-27 01:18:31 +0000

    Added a skeleton for the new import utility for Chapter XX.

---
+++ src-util/edu/stanford/bmir/icd/utils/ImportChapterXX.java
@@ -0,0 +1,20 @@
+package edu.stanford.bmir.icd.utils;
+
+/**
+ * This class imports new content for Chapter XX (External Causes and Injuries)
+ * with values specified in a spreadsheet.
+ * 
+ * @author csnyulas
+ *
+ */
+public class ImportChapterXX {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
