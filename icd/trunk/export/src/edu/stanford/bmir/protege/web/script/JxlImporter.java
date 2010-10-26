68ee5c735ce348bbb2b4c3f0c825a2dd5f6205bd
elliott <elliott@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-10-26 01:51:45 +0000

    Removed all traces of the mapping column names to column names strategy and also removed the error checking that we got with it.

--- icd/trunk/export/src/edu/stanford/bmir/protege/web/script/JxlImporter.java
+++ icd/trunk/export/src/edu/stanford/bmir/protege/web/script/JxlImporter.java
@@ -12,38 +12,32 @@ import org.apache.commons.logging.LogFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 /**
  * Does the actual heavily lifting of reading a csv file of an expected format into a spreadsheet.
- *
- * Column names are specified in the matrix passed in, as are the excel title row and the csv title row.
+ * <p/>
+ * Excel title row and the csv title row are passed in.
  *
  * @author Jack Elliott <jacke@stanford.edu>
  */
-public class JxlImporter implements ExcelImporter{
+public class JxlImporter implements ExcelImporter {
     private static final org.apache.commons.logging.Log logger = LogFactory.getLog(JxlImporter.class);
-    private Map<String, String> csvColumnNamesToExcelColumnNames;
     private Map<String, Map<String, String>> columnValuesMap = new HashMap<String, Map<String, String>>();
     private int excelTitleRow;
     private int csvTitleRow;
-    private Map<String, Integer> excelTitleToColumnNumberMap;
 
     /**
      * The constructor for this class.
      *
-     * @param csvColumnNamesToExcelColumnNames The map from csv columns to excel columns.
-     * @param excelTitleRow  Note that this is array-based (0 is first number), not an excel-based (1 is first number) parameter
-     * @param csvTitleRow    Note that this is an array-based (0 is first number), not an excel-based (1 is first number) parameter.
-     * @param columnValuesMap The values used to map from ICD values to 'pretty' spreadsheet values. 
-     * First Map is Excel column name to map, second map is from input value to 'pretty' spreadsheet value.
+     * @param excelTitleRow   Note that this is array-based (0 is first number), not an excel-based (1 is first number) parameter
+     * @param csvTitleRow     Note that this is an array-based (0 is first number), not an excel-based (1 is first number) parameter.
+     * @param columnValuesMap The values used to map from ICD values to 'pretty' spreadsheet values.
      */
-    public JxlImporter(final Map<String, String> csvColumnNamesToExcelColumnNames, final int excelTitleRow, final int csvTitleRow, Map<String, Map<String, String>> columnValuesMap) {
-        this.csvColumnNamesToExcelColumnNames = csvColumnNamesToExcelColumnNames;
+    public JxlImporter(final int excelTitleRow, final int csvTitleRow, Map<String, Map<String, String>> columnValuesMap) {
         this.excelTitleRow = excelTitleRow;
         this.csvTitleRow = csvTitleRow;
-        if (columnValuesMap != null){
+        if (columnValuesMap != null) {
             this.columnValuesMap = columnValuesMap;
         }
     }
@@ -53,88 +47,65 @@ public class JxlImporter implements ExcelImporter{
         final WritableWorkbook outputWorkbook = Workbook.createWorkbook(new File(outputWorkbookLocation), inputWorkbook);
         int excelCurrentRowNumber = excelTitleRow + 1;
         final WritableSheet sheet = outputWorkbook.getSheet(sheetName);
-        if (sheet == null){
+        if (sheet == null) {
             throw new IllegalArgumentException("Could not find sheet " + sheetName + " in spreadsheet file " + inputWorkbookLocation);
         }
-        excelTitleToColumnNumberMap = mapExcelColumnNamesToColumnNumbers(sheet, excelTitleRow);
         CsvReader reader = new CsvReader(csvLocation, csvTitleRow);
         String nextValue = "";
-        ErrorChecker checker = new ErrorChecker(reader.titles, excelTitleToColumnNumberMap.keySet(), csvColumnNamesToExcelColumnNames.keySet(), new HashSet<String>(csvColumnNamesToExcelColumnNames.values()), 9);
-        if (checker.numberOfErrors()>0){
-            throw new IllegalArgumentException("Can't proceed with data export due to data inconsistencies " + checker.generateReport());
-        }
-        if (checker.numberOfWarnings() > 0){
-            logger.info(checker.generateReport());
-        }
         try {
             while (reader.hasMoreRows()) {
                 reader.nextRow();
                 while (reader.hasMoreColumns()) {
                     nextValue = reader.nextEntry();
                     // if we don't have a title, just assume that we're writing to the same location.
-                    String currentExcelColumnName = csvColumnNamesToExcelColumnNames.get(reader.getCurrentColumnName());
-                    nextValue = mapToExcelValueSet(reader, nextValue, currentExcelColumnName);
-                    if (reader.getCurrentColumnName() == null || reader.getCurrentColumnName().trim().equals("") ) {
-                        writeCell(excelCurrentRowNumber, reader.getCurrentColumn(), sheet, nextValue);
-                    } else {
-                        final String xlColumnName = csvColumnNamesToExcelColumnNames.get(reader.getCurrentColumnName());
-                        final Integer xlColumnNumber = excelTitleToColumnNumberMap.get(xlColumnName);
-                        if (xlColumnNumber != null) {
-                            writeCell(excelCurrentRowNumber, xlColumnNumber, sheet, nextValue);
-                        }
-                    }
+                    nextValue = mapToExcelValueSet(reader, nextValue, Integer.toString(reader.getCurrentColumn()));
+                    writeCell(excelCurrentRowNumber, reader.getCurrentColumn(), sheet, nextValue);
                 }
                 excelCurrentRowNumber++;
             }
         } catch (Exception e) {
-            throw new IllegalArgumentException("caught exception when writing exelCurrentRowNumber=" + excelCurrentRowNumber + ", csvColumnName=" + reader.getCurrentColumnName() +", excelColumnName="+csvColumnNamesToExcelColumnNames.get(reader.getCurrentColumnName())+ " with value " + nextValue, e);
+            throw new IllegalArgumentException("caught exception when writing exelCurrentRowNumber=" + excelCurrentRowNumber + ", csvColumnName=" + reader.getCurrentColumnName() + " with value " + nextValue, e);
         }
         outputWorkbook.write();
         outputWorkbook.close();
     }
 
     private String mapToExcelValueSet(CsvReader reader, String nextValue, String currentExcelColumnName) {
+        StringBuffer sbuff = new StringBuffer();
         if (columnValuesMap.get(currentExcelColumnName) != null) {
-            nextValue = columnValuesMap.get(currentExcelColumnName).get(nextValue);
-            if (nextValue == null){
-                nextValue = reader.currentEntry();
-                if (!nextValue.trim().equals(""))
-                logger.info("Could not find mapping for value '" + nextValue + "' in data sets for excel column " + currentExcelColumnName);
+            final String[] strings = nextValue.split(" \\|\\| ");
+            for (int i = 0; i < strings.length; i++) {
+                nextValue = columnValuesMap.get(currentExcelColumnName).get(strings[i]);
+                if (nextValue == null) {
+                    nextValue = strings[i];
+                    if (!nextValue.trim().equals("")) {
+                        logger.info("Could not find mapping for value '" + nextValue + "' in data sets for excel column " + currentExcelColumnName);
+                    }
+                }
+                sbuff.append(nextValue);
+                if (i + 1 < strings.length) {
+                    sbuff.append(" || ");
+                }
             }
+            nextValue = sbuff.toString();
         }
         return nextValue;
     }
 
     private void writeCell(int rowNumber, int columnNumber, WritableSheet sheet, String nextValue) throws WriteException {
-        if (nextValue == null || nextValue.trim().equals("")){
-            return ;
+        if (nextValue == null || nextValue.trim().equals("")) {
+            return;
         }
 
         final WritableCell writableCell = sheet.getWritableCell(columnNumber, rowNumber);
         Label label = new Label(columnNumber, rowNumber, nextValue);
-        if (writableCell.getCellFormat()!=null){
+        if (writableCell.getCellFormat() != null) {
             label.setCellFormat(writableCell.getCellFormat());
         }
         sheet.addCell(label);
         if (writableCell.getWritableCellFeatures() != null) {
             label.setCellFeatures(writableCell.getWritableCellFeatures());
         }
-    }
-
-    public Map<String, Integer> mapExcelColumnNamesToColumnNumbers(final WritableSheet sheet, Integer titleRow){
-        Map<String,Integer> titleToColumnMap = new HashMap<String, Integer>();
-        for (int columnNumber = 0; columnNumber < 50; columnNumber ++){
-        final WritableCell writableCell = sheet.getWritableCell(columnNumber, titleRow);
-            titleToColumnMap.put(writableCell.getContents(), columnNumber);
-        }
-        return titleToColumnMap;
-    }
-
-    public Integer getColumnNumber(String name){
-        if (excelTitleToColumnNumberMap == null ){
-            throw new IllegalStateException("columns not yet mapped");
-        }
-        return excelTitleToColumnNumberMap.get(name);
     }
 
 }
--- icd/trunk/export/test/edu/stanford/bmir/protege/web/script/ICDImporterTest.java
+++ icd/trunk/export/test/edu/stanford/bmir/protege/web/script/ICDImporterTest.java
@@ -17,31 +17,22 @@ public class ICDImporterTest extends TestCase {
     private final String templateLocation = "excel/template.xls";
     private final String sheetName = "Authoring template";
     private ExcelImporter importer = new ICDImporter();
-
-    public void testDetailedDefinitionPopulated() throws BiffException, IOException, WriteException {
-        final String outputWorkbookLocation = "output/output.xls";
-        importer.importFile("test/sample-detailed-definition-populated-file.csv", templateLocation, outputWorkbookLocation, sheetName);
-        final Workbook outputWorkbook = Workbook.getWorkbook(new File(outputWorkbookLocation));
-        final Sheet sheet = outputWorkbook.getSheet(sheetName);
-        System.out.println("getCellContents(sheet, 15, 3) = " + getCellContents(sheet, "Definition (plus citation in square brackets [ ] at end)", 3));
-        assertTrue(getCellContents(sheet, "Definition (plus citation in square brackets [ ] at end)", 3).trim().length()==0);
-        assertTrue(getCellContents(sheet, "Detailed Definition", 3).equals("my long definition"));
-    }
+    private static final int TEXTUAL_DEFINITION_COLUMN = 16;
+    private static final int DETAILED_DEFINITION_COLUMN = 17;
 
     public void testTextualDefinitionPopulated() throws BiffException, IOException, WriteException {
         final String outputWorkbookLocation = "output/output.xls";
         importer.importFile("test/sample-textual-definition-populated-file.csv", templateLocation, outputWorkbookLocation, sheetName);
         final Workbook outputWorkbook = Workbook.getWorkbook(new File(outputWorkbookLocation));
         final Sheet sheet = outputWorkbook.getSheet(sheetName);
-        System.out.println("getCellContents(sheet, 15, 3) = " + getCellContents(sheet, "Definition (plus citation in square brackets [ ] at end)", 3));
         assertEquals("Acral lentiginous malignant melanoma is a distinct form of melanoma occurring on palms and soles or in or around the nail apparatus.  It is typically preceded by a slowly progressive in situ phase (acral lentiginous melanoma in situ) before it becomes invasive.   It may, however, present de novo with a rapidly growing invasive tumour. Although it represents a small proportion of melanomas overall (5 to 10%), it accounts for a high proportion of melanomas seen in dark-skinned people.  On the palms and soles it presents typically as an area of irregular macular pigmentation.  If it develops in the nail matrix it may present in the early stages as a longitudinal pigmented band within the nail plate. More aggressive tumours present as ulcerated nodules which, when involving the nail apparatus, can cause destruction of the nail plate.",
-                getCellContents(sheet, "Definition (plus citation in square brackets [ ] at end)", 3));
-        assertTrue(getCellContents(sheet, "Detailed Definition", 3).trim().equals(""));
+                getCellContents(sheet, TEXTUAL_DEFINITION_COLUMN, 3));
+        assertTrue(getCellContents(sheet, DETAILED_DEFINITION_COLUMN, 3).trim().equals(""));
     }
 
 
-    private String getCellContents(final Sheet sheet, String columnName, int row){
-        Cell cell = sheet.getCell(importer.getColumnNumber(columnName),  row);
+    private String getCellContents(final Sheet sheet, int column, int row) {
+        Cell cell = sheet.getCell(column, row);
         return cell.getContents();
     }
 }
