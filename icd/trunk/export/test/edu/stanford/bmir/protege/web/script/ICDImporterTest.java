9b5798911818bd95098a7c7676309c72e36120d3
elliott <elliott@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-10-29 01:18:05 +0000

    Committing new test cases that verify our output to the excel file.

--- icd/trunk/export/test/edu/stanford/bmir/protege/web/script/ICDImporterTest.java
+++ icd/trunk/export/test/edu/stanford/bmir/protege/web/script/ICDImporterTest.java
@@ -1,38 +1,32 @@
 package edu.stanford.bmir.protege.web.script;
 
-import junit.framework.TestCase;
-import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
-import jxl.read.biff.BiffException;
-import jxl.write.WriteException;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
+ * Test for the ICDImporter.
+ * <p/>
+ * If this test does not work, go and run the ExportScriptWrapperTest on who's output it depends!
+ *
  * @author Jack Elliott <jacke@stanford.edu>
  */
-public class ICDImporterTest extends TestCase {
+public class ICDImporterTest extends ValuesTest {
     private final String templateLocation = "excel/template.xls";
     private final String sheetName = "Authoring template";
     private ExcelImporter importer = new ICDImporter();
-    private static final int TEXTUAL_DEFINITION_COLUMN = 16;
-    private static final int DETAILED_DEFINITION_COLUMN = 17;
+    private Sheet sheet;
 
-    public void testTextualDefinitionPopulated() throws BiffException, IOException, WriteException {
-        final String outputWorkbookLocation = "output/output.xls";
-        importer.importFile("test/sample-textual-definition-populated-file.csv", templateLocation, outputWorkbookLocation, sheetName);
-        final Workbook outputWorkbook = Workbook.getWorkbook(new File(outputWorkbookLocation));
-        final Sheet sheet = outputWorkbook.getSheet(sheetName);
-        assertEquals("Acral lentiginous malignant melanoma is a distinct form of melanoma occurring on palms and soles or in or around the nail apparatus.  It is typically preceded by a slowly progressive in situ phase (acral lentiginous melanoma in situ) before it becomes invasive.   It may, however, present de novo with a rapidly growing invasive tumour. Although it represents a small proportion of melanomas overall (5 to 10%), it accounts for a high proportion of melanomas seen in dark-skinned people.  On the palms and soles it presents typically as an area of irregular macular pigmentation.  If it develops in the nail matrix it may present in the early stages as a longitudinal pigmented band within the nail plate. More aggressive tumours present as ulcerated nodules which, when involving the nail apparatus, can cause destruction of the nail plate.",
-                getCellContents(sheet, TEXTUAL_DEFINITION_COLUMN, 3));
-        assertTrue(getCellContents(sheet, DETAILED_DEFINITION_COLUMN, 3).trim().equals(""));
+    @Override
+    protected String getFieldContents(int fieldPosition, int rowNumber) {
+        return sheet.getCell(fieldPosition, rowNumber).getContents();
     }
 
-
-    private String getCellContents(final Sheet sheet, int column, int row) {
-        Cell cell = sheet.getCell(column, row);
-        return cell.getContents();
+    @Override
+    protected void initializeTest(final String topNode, final String csvFileName, final String outputWorkbookLocation) throws Exception {
+        importer.importFile(csvFileName, templateLocation, outputWorkbookLocation, sheetName);
+        final Workbook outputWorkbook = Workbook.getWorkbook(new File(outputWorkbookLocation));
+        sheet = outputWorkbook.getSheet(sheetName);
     }
 }
