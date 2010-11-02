8494eebc02dcae2fdbe4cd3143fb97b936855f12
elliott <elliott@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-11-02 19:03:23 +0000

    Capitalizing constants.

--- icd/trunk/export/src/edu/stanford/bmir/protege/icd/export/script/RunICDImporter.java
+++ icd/trunk/export/src/edu/stanford/bmir/protege/icd/export/script/RunICDImporter.java
@@ -12,25 +12,25 @@ import java.util.Arrays;
  * @author Jack Elliott <jacke@stanford.edu>
  */
 public class RunICDImporter {
-    private static final String csvLocationParameter = "csvLocation";
-    private static final String inputWorkbookLocationParameter = "inputWorkbookLocation";
-    private static final String outputWorkbookLocationParameter = "outputWorkbookLocation";
-    private static final String sheetNameParameter = "sheetName";
+    private static final String CSV_LOCATION_PARAMETER = "csvLocation";
+    private static final String INPUT_WORKBOOK_LOCATION_PARAMETER = "inputWorkbookLocation";
+    private static final String OUTPUT_WORKBOOK_LOCATION_PARAMETER = "outputWorkbookLocation";
+    private static final String SHEET_NAME_PARAMETER = "sheetName";
     private static final org.apache.commons.logging.Log logger = LogFactory.getLog(RunExportScriptWrapper.class);
 
     public static void main(String[] args) {
         try {
             Options options = new Options();
-            options.addOption(csvLocationParameter, true, "the location of the csv file");
-            options.addOption(inputWorkbookLocationParameter, true, "the location of the outputWorkbook file to merge");
-            options.addOption(outputWorkbookLocationParameter, true, "the final location of the merged excel file");
-            options.addOption(sheetNameParameter, true, "the name of the sheet to use");
+            options.addOption(CSV_LOCATION_PARAMETER, true, "the location of the csv file");
+            options.addOption(INPUT_WORKBOOK_LOCATION_PARAMETER, true, "the location of the outputWorkbook file to merge");
+            options.addOption(OUTPUT_WORKBOOK_LOCATION_PARAMETER, true, "the final location of the merged excel file");
+            options.addOption(SHEET_NAME_PARAMETER, true, "the name of the sheet to use");
             CommandLineParser parser = new PosixParser();
             CommandLine cmd = parser.parse(options, args);
-            final String csvLocation = cmd.getOptionValue(csvLocationParameter);
-            final String inputWorkbookLocation = cmd.getOptionValue(inputWorkbookLocationParameter);
-            final String outputWorkbookLocation = cmd.getOptionValue(outputWorkbookLocationParameter);
-            final String sheetName = cmd.getOptionValue(sheetNameParameter);
+            final String csvLocation = cmd.getOptionValue(CSV_LOCATION_PARAMETER);
+            final String inputWorkbookLocation = cmd.getOptionValue(INPUT_WORKBOOK_LOCATION_PARAMETER);
+            final String outputWorkbookLocation = cmd.getOptionValue(OUTPUT_WORKBOOK_LOCATION_PARAMETER);
+            final String sheetName = cmd.getOptionValue(SHEET_NAME_PARAMETER);
             CsvToExcelConverter converter = new ICDCsvToExcelConverter();
             converter.convertFile(csvLocation, inputWorkbookLocation, outputWorkbookLocation, sheetName);
         } catch (Exception e) {
