2a71a78e38bc97d2e55e32598b4aab2dcd1d054c
nyulas <2441046+csnyulas@users.noreply.github.com>
2010-11-20 00:56:20 +0000

    Replaced "/" with File.separator so that file paths are OS-independent

--- export/src/edu/stanford/bmir/protege/icd/export/FileUtils.java
+++ export/src/edu/stanford/bmir/protege/icd/export/FileUtils.java
@@ -15,7 +15,7 @@ public class FileUtils {
     public static InputStream getInputStream(String location) throws FileNotFoundException {
         File file = new File(location);
         if (!file.exists()) {
-            file = new File(PluginUtilities.getInstallationDirectory(ICDExporterPlugin.class.getName()).getPath() + "/" + location);
+            file = new File(PluginUtilities.getInstallationDirectory(ICDExporterPlugin.class.getName()).getPath() + File.separator + location);
             if (!file.exists()) {
                 throw new IllegalArgumentException("Could not find file " + file.getAbsolutePath() + " on path.");
             }
