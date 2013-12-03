21dd0a30bf21c1f43ecc315a952f5d96c9dd50f3
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2013-12-03 23:11:50 +0000

    another intermediary commit for this class, not ready for prime time yet

--- icd/trunk/src/edu/stanford/bmir/icd/claml/SiblingReordering.java
+++ icd/trunk/src/edu/stanford/bmir/icd/claml/SiblingReordering.java
@@ -1,6 +1,6 @@
 package edu.stanford.bmir.icd.claml;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.SortedMap;
@@ -47,6 +47,7 @@ public class SiblingReordering {
 
     public List<RDFSNamedClass> getOrderedChildren(RDFSNamedClass parent) {
         SortedMap<Integer, RDFSNamedClass> orderedChildrenMap = new TreeMap<Integer, RDFSNamedClass>();
+        computeOrderedChildrenSortedMap(parent, orderedChildrenMap);
         return (List<RDFSNamedClass>) orderedChildrenMap.values();
     }
 
@@ -63,8 +64,14 @@ public class SiblingReordering {
      *  <li>null -> child</li>
      *  <li>index -> null</li>
      *  <li>index -> child that is not a child</li>
+     *  <li>index1 = index2</li>
      *  <li>missing index for an existing child</li>
      *  </ul>
+     *  </p>
+     *
+     *  <p>
+     *  If index is found to be invalid, the method will try to fix it in place,
+     *  i.e, add the bad index entries at the end of the index.
      *  </p>
      *
      * @param parent
@@ -84,50 +91,22 @@ public class SiblingReordering {
             return false;
         }
 
-        //FIXME:just check if index is valid, if not, regenerated everything
+        isValidIndex = fillOrderedMap(childrenIndex, orderedChildrenMap);
 
-        // all the children which should be indexed
-        List<RDFSNamedClass> unorderedChildren = cm.getRDFSNamedClassList(parent.getSubclasses(false));
+        // the real subclasses that should also show up in the index
+        List<RDFSNamedClass> realChildren = cm.getRDFSNamedClassList(parent.getSubclasses(false));
 
-        // children that have a corrupted index entry, e.g., index = null
-        List<RDFSNamedClass> unindexedChildren = new ArrayList<RDFSNamedClass>();
+        // the values are backed by the map, any operation on this will affect the map
+        Collection<RDFSNamedClass> backedChildrenInIndex = orderedChildrenMap.values();
+        isValidIndex = backedChildrenInIndex.retainAll(realChildren) && isValidIndex; //keep in the index only the children that are real
 
-        for (RDFResource childIndexInstance : childrenIndex) {
-            Integer index = (Integer) childIndexInstance.getPropertyValue(cm.getOrderedChildIndexProperty());
-            RDFSNamedClass child = (RDFSNamedClass) childIndexInstance.getPropertyValue(cm.getOrderedChildProperty());
-
-            //corrupted index, ignore this index instance for now, it will be fixed next time a reindex will occur (e.g., ordering sibling, move, create)
-            if (index == null) {
-                if (child != null) {
-                    unindexedChildren.add(child);
-                }
-                isValidIndex = false;
-            }
-
-            if (unorderedChildren.contains(child)) {
-                orderedChildrenMap.put(index, child);
-                unorderedChildren.remove(child);
-            } else {
-                isValidIndex = false;
-            }
-        }
-
-        if (unorderedChildren.size() > 0) {
+        //likely never true, if we assume that index corruption rarely happens
+        if (backedChildrenInIndex.size() != realChildren.size()) {
             isValidIndex = false;
-        }
 
-        //add to the remaining unordered children, the children with corrupted indexes, and sort them
-        unorderedChildren.addAll(unindexedChildren);
-        Collections.sort(unorderedChildren, new FrameComparator<Frame>());
-
-        int lastIndex = orderedChildrenMap.lastKey();
-        //round it to the next million
-        lastIndex = (lastIndex + CHILD_INDEX_INCREMENT) / CHILD_INDEX_INCREMENT;
-
-        //add the unordered children to the index
-        for (RDFSNamedClass child : unorderedChildren) {
-            orderedChildrenMap.put(lastIndex, child);
-            lastIndex = lastIndex + CHILD_INDEX_INCREMENT;
+            //keep real children that are not in the index
+            realChildren.removeAll(backedChildrenInIndex);
+            addChildrenToVirtualIndex(realChildren, orderedChildrenMap);
         }
 
         if (isValidIndex == false) {
@@ -135,6 +114,40 @@ public class SiblingReordering {
         }
 
         return isValidIndex;
+    }
+
+
+    private boolean fillOrderedMap(List<RDFResource> childrenIndex, SortedMap<Integer, RDFSNamedClass> orderedChildrenMap) {
+        boolean isValidIndex = true;
+
+        //fill sorted map with whatever is in the index, even corrupted stuff
+        for (RDFResource childIndexInstance : childrenIndex) {
+            Integer index = (Integer) childIndexInstance.getPropertyValue(cm.getOrderedChildIndexProperty());
+            RDFSNamedClass child = (RDFSNamedClass) childIndexInstance.getPropertyValue(cm.getOrderedChildProperty());
+
+            if (index != null) { //avoid a NPE
+                orderedChildrenMap.put(index, child);
+            } else {
+                isValidIndex = false;
+            }
+        }
+
+        return isValidIndex;
+    }
+
+    private void addChildrenToVirtualIndex(List<RDFSNamedClass> childrenToAdd, SortedMap<Integer, RDFSNamedClass> orderedChildrenMap) {
+        Collections.sort(childrenToAdd, new FrameComparator<Frame>());
+
+        int lastIndex = orderedChildrenMap.lastKey();
+        //round it to the next million
+        lastIndex = ((lastIndex - 1) / CHILD_INDEX_INCREMENT + 1) * CHILD_INDEX_INCREMENT; //check for out of range
+
+        //add the unordered children to the index
+        for (RDFSNamedClass child : childrenToAdd) {
+            orderedChildrenMap.put(lastIndex, child);
+            lastIndex = lastIndex + CHILD_INDEX_INCREMENT;
+            //TODO: check for negative numbers!!!!
+        }
     }
 
     public boolean reorderSibling(RDFSNamedClass movedCls, RDFSNamedClass targetCls, boolean isBelow,
