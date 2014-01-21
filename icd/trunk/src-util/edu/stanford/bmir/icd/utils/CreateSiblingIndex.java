d494b1c3c65aa5b206fd0c33286be967e1d6d66e
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2014-01-21 22:35:04 +0000

    script for creating sibling index for all icd cats

---
+++ icd/trunk/src-util/edu/stanford/bmir/icd/utils/CreateSiblingIndex.java
@@ -0,0 +1,65 @@
+package edu.stanford.bmir.icd.utils;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import edu.stanford.bmir.icd.claml.ICDContentModel;
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
+
+public class CreateSiblingIndex {
+
+    private static OWLModel owlModel;
+    private static ICDContentModel cm;
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Needs 1 params: ICD pprj file");
+            return;
+        }
+
+        String fileName = args[0];
+
+        List errors = new ArrayList();
+        Project prj = Project.loadProjectFromFile(fileName , errors);
+        if (errors.size() > 0) {
+            System.out.println("There were errors at loading project: " + fileName);
+            System.exit(1);
+        }
+
+        owlModel = (OWLModel) prj.getKnowledgeBase();
+        cm = new ICDContentModel(owlModel);
+
+        createSiblingIndex();
+
+    }
+
+    private static void createSiblingIndex() {
+        Collection<RDFSNamedClass> cats = cm.getICDCategories();
+        cats.add(cm.getICDCategoryClass());
+
+        Log.getLogger().info("Got: " + cats.size() + " categories. Date: " + new Date());
+
+
+        int i = 0;
+        for (RDFSNamedClass cat : cats) {
+            if (cat.getSubclassCount() > 0) {
+                createSiblingIndex(cat);
+            }
+            if (i % 1000 == 0) {
+                Log.getLogger().info("Processed " + i + " cats. Date: " + new Date());
+            }
+            i ++;
+        }
+
+    }
+
+    private static void createSiblingIndex(RDFSNamedClass cat) {
+        cm.checkIndexAndRecreate(cat, true);
+    }
+}
