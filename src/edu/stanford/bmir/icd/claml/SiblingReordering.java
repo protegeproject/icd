301847e9c3765fbe19245ab33809f9113a9d44b6
Tania Tudorache <tudorache@users.noreply.github.com>
2014-03-22 05:31:52 +0000

    - fix for a very weird null pointer exception of the logger.. using isLoggable now.

--- src/edu/stanford/bmir/icd/claml/SiblingReordering.java
+++ src/edu/stanford/bmir/icd/claml/SiblingReordering.java
@@ -171,7 +171,7 @@ public class SiblingReordering {
         }
 
         if (isValidIndex == false) {
-            if (Level.FINE.equals(log.getLevel())) {
+            if (log.isLoggable(Level.FINE)) {
                 log.fine("Invalid sibling index for class: " + parent.getName() + " Browser text: " + parent.getBrowserText());
             }
         }
