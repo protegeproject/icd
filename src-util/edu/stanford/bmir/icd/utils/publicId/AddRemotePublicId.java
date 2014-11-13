ad72606d7afded3e2f985fb718a9210a3d0108d6
Tania Tudorache <tudorache@users.noreply.github.com>
2014-11-13 20:39:53 +0000

    removed protege job - needs to be also on server

--- src-util/edu/stanford/bmir/icd/utils/publicId/AddRemotePublicId.java
+++ src-util/edu/stanford/bmir/icd/utils/publicId/AddRemotePublicId.java
@@ -6,12 +6,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.stanford.bmir.whofic.icd.ICDContentModel;
-import edu.stanford.smi.protege.exception.ProtegeException;
-import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.server.RemoteProjectManager;
 import edu.stanford.smi.protege.util.Log;
-import edu.stanford.smi.protege.util.ProtegeJob;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
@@ -30,14 +27,14 @@ public class AddRemotePublicId {
         Project prj = connectToRemoteProject(args);
 
         if (prj == null) {
-            log.log(Level.SEVERE, "Cannot connect to remote project: " + args);
+            log.log(Level.SEVERE, "Cannot connect to remote project: " + args[0] +" " + args[1] + " " + args[2] + " " + args[3]);
             return;
         }
 
         OWLModel owlModel = (OWLModel) prj.getKnowledgeBase();
 
         if (owlModel == null) {
-            log.severe("Failed to load remote project: " + args);
+            log.severe("Failed to load remote project: " + args[0] +" " + args[1] + " " + args[2] + " " + args[3]);
             return;
         }
 
@@ -49,14 +46,18 @@ public class AddRemotePublicId {
             return;
         }
 
+        init(owlModel);
+
         log.info("Started remote job at:" + new Date());
         long t0 = System.currentTimeMillis();
 
-        try{
+/*        try{
             new AddPublicIdOnServer(owlModel).execute();
         } catch (Throwable t) {
             Log.getLogger().log(Level.WARNING, "Error at executing remote job", t);
         }
+*/
+        addPublicId();
 
         log.info("Ended remote job at:" + new Date());
         log.info("Task took: " + (System.currentTimeMillis() - t0)/1000 +" secs.");
@@ -76,27 +77,30 @@ public class AddRemotePublicId {
     }
 
 
-    static class AddPublicIdOnServer extends ProtegeJob {
+//    static class AddPublicIdOnServer extends ProtegeJob {
 
-        private static final long serialVersionUID = 7895759535282049363L;
+//        private static final long serialVersionUID = 7895759535282049363L;
 
         private static ICDContentModel icdContentModel;
         private static RDFProperty publicIDProp;
 
-        public AddPublicIdOnServer(KnowledgeBase kb) {
+  /*      public AddPublicIdOnServer(KnowledgeBase kb) {
             super(kb);
         }
 
 
         @Override
         public Object run() throws ProtegeException {
-            icdContentModel = new ICDContentModel((OWLModel)getKnowledgeBase());
+            init((OWLModel)getKnowledgeBase());
+            return null;
+        }
+*/
+        private static void init(OWLModel owlModel){
+            icdContentModel = new ICDContentModel(owlModel);
             publicIDProp = icdContentModel.getPublicIdProperty();
 
             addPublicId();
-            return null;
         }
-
 
         private static void addPublicId() {
             Log.getLogger().info("Started get ICD cats at: " + new Date());
@@ -131,7 +135,7 @@ public class AddRemotePublicId {
                     Log.getLogger().warning("Could not get public ID from ID server for class: " + cat.getName());
                 } else {
                     cat.setPropertyValue(publicIDProp, publicId);
-                    Log.getLogger().info("Adding public id to class: " + cat.getName() + " publicId: " + publicId);
+                    //Log.getLogger().info("Adding public id to class: " + cat.getName() + " publicId: " + publicId);
                     return true;
                 }
             } catch (Exception e) {
@@ -140,6 +144,6 @@ public class AddRemotePublicId {
             return false;
         }
 
-    }
+//    } // end Protege job
 
 }
