afeafae25856783a76938e52a245086e6d998c45
nyulas <nyulas@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2020-01-15 13:57:09 +0000

    Initial commit of the DeleteREtiredClasses script.

---
+++ icd/trunk/src-util/edu/stanford/bmir/icd/utils/DeleteRetiredClasses.java
@@ -0,0 +1,171 @@
+package edu.stanford.bmir.icd.utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.apache.log4j.Logger;
+
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.ui.ProjectManager;
+import edu.stanford.smi.protegex.owl.model.OWLClass;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+
+public class DeleteRetiredClasses {
+	private static transient Logger log = Logger.getLogger(DeleteRetiredClasses.class);
+
+	private static final String COL_SEPARATOR = "\t";
+	private static final int CLASS_COUNT_FOR_STATUS_NOTIFICATION = 10;
+	private static int currClassCount = 0;
+	
+	public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Expected two arguments: pprj_file_name  file_containing_list_of_classes_to_be_deleted");
+            return;
+        }
+
+        long startTime = System.currentTimeMillis();
+        
+        Collection<?> errors = new ArrayList<Object>();
+        Project prj = Project.loadProjectFromFile(args[0], errors);
+
+        if (errors != null) {
+            ProjectManager.getProjectManager().displayErrors("Errors", errors);
+        }
+
+        OWLModel owlModel = (OWLModel) prj.getKnowledgeBase();
+
+        List<String> classes = readList(args[1]);
+        deleteClasses(owlModel, classes);
+        
+        long timeElapsed = System.currentTimeMillis() - startTime;
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        System.out.println(String.format("Deleted % d classes. Total time: %d mins %.3f seconds (or %s)", 
+        		currClassCount,
+        		timeElapsed / (60*1000), timeElapsed / 1000.0,
+        		sdf.format(timeElapsed)));
+	}
+
+	
+	private static List<String> readList(String csvFile) {
+		BufferedReader csvReader = null;
+		
+		try {
+			csvReader = new BufferedReader(new FileReader(csvFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		List<String> classesToBeDeleted = new ArrayList<String>();
+		
+		String row = null;
+		try {
+			while (( row = csvReader.readLine()) != null) {
+				processLine(row, classesToBeDeleted);
+			}
+		} catch (IOException e) {
+			log.error("IO Exception at processing FMA row: " + row, e);
+		}
+
+		try {
+			csvReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return classesToBeDeleted;
+	}
+
+	
+	private static void processLine(String row, List<String> list) {
+		String[] data = row.split(COL_SEPARATOR);
+		
+		//we ignore anything that is not in the first column
+		list.add(data[0]);
+	}
+
+	
+	private static void deleteClasses(OWLModel owlModel, List<String> classes) {
+		
+		boolean genEvents = owlModel.setGenerateEventsEnabled(false);
+		
+		try {
+			List<OWLClass> owlClasses = new LinkedList<OWLClass>();
+			for (String className : classes) {
+				OWLNamedClass owlClass = owlModel.getOWLNamedClass(className);
+				if (owlClass == null) {
+					log.warn(className + " not found. Cannot be deleted.");
+					System.out.println(className + " not found. Cannot be deleted.");
+				}
+				else {
+					owlClasses.add(owlClass);
+				}
+			}
+			
+			while (! owlClasses.isEmpty()) {
+				OWLClass owlClass = owlClasses.get(0);
+				deleteClass(owlModel, owlClass, owlClasses);
+			}
+		}
+		finally {
+			owlModel.setGenerateEventsEnabled(genEvents);
+		}
+	}
+
+
+	@SuppressWarnings("unchecked")
+	private static void deleteClass(OWLModel owlModel, OWLClass owlClass, List<OWLClass> owlClasses) {
+		owlClasses.remove(owlClass);
+		if (owlClass.isDeleted()) {
+			return;
+		}
+		
+		Collection<OWLClass> directSubclasses = (Collection<OWLClass>) owlClass.getSubclasses(false);
+		
+		Iterator<OWLClass> it = directSubclasses.iterator();
+		while (it.hasNext()) {
+			OWLClass subclass = it.next();
+			if (owlClasses.contains(subclass)) {
+				deleteClass(owlModel, subclass, owlClasses);
+			}
+		}
+		
+		try {
+			directSubclasses = (Collection<OWLClass>) owlClass.getSubclasses(false);
+			
+			if (directSubclasses.isEmpty()) {
+				System.out.println("Deleting class " + owlClass + " (" + owlClass.getBrowserText() + ")");
+				owlClass.delete();
+			}
+			else {
+				directSubclasses.stream().forEach((c) -> removeParentRelation(c, owlClass));
+			}
+	
+			if (++currClassCount % CLASS_COUNT_FOR_STATUS_NOTIFICATION == 0) {
+				System.out.println(String.format("Deleted: %d. %d to go.", currClassCount, owlClasses.size()));
+			}
+		}
+		catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	private static void removeParentRelation(OWLClass child, OWLClass parent) {
+		child.removeSuperclass(parent);
+		System.out.println("Removing superclass " + parent + " (" + parent.getBrowserText() + ")" +
+				" of class " + child + " (" + child.getBrowserText() + ")");
+	}
+
+
+}
