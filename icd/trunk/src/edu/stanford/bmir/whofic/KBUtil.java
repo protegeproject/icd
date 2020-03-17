9c410f0df760cacb0b354f912245fc18128fe093
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2020-03-17 12:39:06 +0000

    first step in fixing the id issue

--- icd/trunk/src/edu/stanford/bmir/whofic/KBUtil.java
+++ icd/trunk/src/edu/stanford/bmir/whofic/KBUtil.java
@@ -1,28 +1,114 @@
 package edu.stanford.bmir.whofic;
 
 import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protegex.owl.model.OWLIndividual;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+import edu.stanford.smi.protegex.owl.model.OWLProperty;
+import edu.stanford.smi.protegex.owl.model.RDFIndividual;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.RDFSClass;
 import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
 
 public class KBUtil {
 
-	// This method will try to get the class with the full URI using the 
-	// Protege OWL API, and if it fails, it will try to get it through the 
-	// Frames API with the provided name (probably the short id)
-	public static RDFSNamedClass getRDFSNamedClass(OWLModel owlModel, String clsName) {
-		RDFSNamedClass cls = owlModel.getRDFSNamedClass(clsName);
+	/**
+	 * This is a workaround for the problem that
+	 * many iCAT ids are not absolute URIs, and the owlModel.getResource(name) 
+	 * will try to expand these names, and will fail.
+	 * 
+	 * This method will return the RDFResource, even if the provided name is 
+	 * the long or short id, and whether in the database we have stored
+	 * long or short ids.
+	 * 
+	 * @param owlModel - the OWL Model
+	 * @param name - the short of long name of the resource to be retrieved
+	 * @return
+	 */
+	public static RDFResource getRDFResource(OWLModel owlModel, String name) {
+		RDFResource res = owlModel.getRDFResource(name);
 		
-		if (cls == null) { //maybe short name is used
-			Cls framesCls = ((KnowledgeBase)owlModel).getCls(clsName);
-			if (framesCls != null && framesCls instanceof RDFSNamedClass) {
-				cls = (RDFSNamedClass) framesCls;
-			}
+		if(res != null) { //this is the good case
+			return res;
 		}
 		
-		return cls;
+		//this is the fallback for non-absolute URIs (short ids)
+		@SuppressWarnings("deprecation")
+		Frame frame = owlModel.getFrame(name);
+		
+		return frame != null && frame instanceof RDFResource ? 
+				(RDFResource) frame : null;
 	}
-	
-	
+
+	public static RDFSNamedClass getRDFSNamedClass(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof RDFSNamedClass ? 
+				(RDFSNamedClass) res : null;
+	}
+
+	public static OWLNamedClass getOWLNamedClass(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof OWLNamedClass ? 
+				(OWLNamedClass) res : null;
+	}
+
+	public static RDFSClass getRDFSClass(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof RDFSClass ? 
+				(RDFSClass) res : null;
+	}
+
+	public static RDFProperty getRDFProperty(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof RDFProperty ? 
+				(RDFProperty) res : null;
+	}
+
+	public static OWLProperty getOWLProperty(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof OWLProperty ? 
+				(OWLProperty) res : null;
+	}
+
+	public static RDFIndividual getRDFIndividual(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof RDFIndividual ? 
+				(RDFIndividual) res : null;
+	}
+
+	public static OWLIndividual getOWLIndividual(OWLModel owlModel, String name) {
+		RDFResource res = getRDFResource(owlModel, name);
+		return res != null && res instanceof OWLIndividual ? 
+				(OWLIndividual) res : null;
+	}
+
+	public static Cls getCls(KnowledgeBase kb, String name) {
+		if (kb instanceof OWLModel) {
+			return getRDFSClass((OWLModel)kb, name);
+		}
+		
+		return kb.getCls(name);
+	}
+
+	public static Slot getSlot(KnowledgeBase kb, String name) {
+		if (kb instanceof OWLModel) {
+			return getRDFProperty((OWLModel)kb, name);
+		}
+		
+		return kb.getSlot(name);
+	}
+
+	public static Instance getInstance(KnowledgeBase kb, String name) {
+		if (kb instanceof OWLModel) {
+			return getRDFResource((OWLModel)kb, name);
+		}
+		
+		return kb.getInstance(name);
+	}
 	
 }
