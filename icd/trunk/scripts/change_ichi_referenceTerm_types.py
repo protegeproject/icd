eb5ce47b52937551589d2c5d5572f710aa87ea0f
nyulas <nyulas@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2022-11-25 12:20:03 +0000

    Updated scripts for ICHI model changes.

--- icd/trunk/scripts/change_ichi_referenceTerm_types.py
+++ icd/trunk/scripts/change_ichi_referenceTerm_types.py
@@ -16,10 +16,12 @@ awRefTerm = kb.getOWLNamedClass("http://who.int/icd#AssociatedWithReferenceTerm"
 
 tRefTerm = kb.getOWLNamedClass("http://who.int/icd#TargetReferenceTerm");
 aRefTerm = kb.getOWLNamedClass("http://who.int/icd#ActionReferenceTerm");
-mRefTerm = kb.getOWLNamedClass("http://who.int/icd#MeanReferenceTerm");
+mRefTerm = kb.getOWLNamedClass("http://who.int/icd#MeansReferenceTerm");
 atRefTerm = kb.getOWLNamedClass("http://who.int/icd#AdditionalTargetReferenceTerm");
 
 import sys
+import java.util.ArrayList as ArrayList
+
 for c in [t]:
 #for c in classes:
   print c.toString() + ": ";
@@ -30,13 +32,39 @@ for c in [t]:
   print
   sys.stdout.flush();
 
-#for c in [t]:
-for c in classes:
-  print c.toString() + " (" + c.getBrowserText() + "): ";
-  sys.stdout.flush();
+#working with buckets instead of full set
+arr = ArrayList(classes)
 
+bucketSize = 5000;
+n= classes.size()
+groups = range(1, n / bucketSize);
+print groups;
+grIndex = 0;
+
+#repeat manually for all group numbers (then do the last iteration below):
+
+print "Group: " + str(groups[grIndex]);
+subset = ArrayList()
+for i in range(bucketSize * (groups[grIndex] - 1), bucketSize * groups[grIndex]) :
+  subset.add(arr.get(i));
+grIndex = grIndex + 1;
+
+
+#last:
+#print "(Last) Group: " + str(groups[grIndex-1] + 1);
+#subset = ArrayList()
+#for i in range(bucketSize * (groups[grIndex-1]), classes.size()) :
+#  subset.add(arr.get(i));
+
+
+#for c in [t]:
+#for c in classes:
+print "Processing " + str(subset.size()) + " classes";
+for c in subset:
   refTerms = c.getPropertyValues(pT);
   if refTerms.size() > 0 :
+      print c.toString() + " (" + c.getBrowserText() + "): ";
+      sys.stdout.flush();
       print ".   hasTarget:";
       for refTerm in refTerms:
         s = ".    .  " + refTerm.toString();
@@ -49,6 +77,8 @@ for c in classes:
 
   refTerms = c.getPropertyValues(pA);
   if refTerms.size() > 0 :
+      print c.toString() + " (" + c.getBrowserText() + "): ";
+      sys.stdout.flush();
       print ".   hasAction:";
       for refTerm in refTerms:
         s = ".    .  " + refTerm.toString();
@@ -61,6 +91,8 @@ for c in classes:
 
   refTerms = c.getPropertyValues(pM);
   if refTerms.size() > 0 :
+      print c.toString() + " (" + c.getBrowserText() + "): ";
+      sys.stdout.flush();
       print ".   hasMeans:";
       for refTerm in refTerms:
         s = ".    .  " + refTerm.toString();
@@ -73,6 +105,8 @@ for c in classes:
 
   refTerms = c.getPropertyValues(pAT);
   if refTerms.size() > 0 :
+      print c.toString() + " (" + c.getBrowserText() + "): ";
+      sys.stdout.flush();
       print ".   additionalTarget:";
       for refTerm in refTerms:
         s = ".    .  " + refTerm.toString();
@@ -82,3 +116,4 @@ for c in classes:
           s = s + "-->" + refTerm.toString();
         print s;
         sys.stdout.flush();
+print "Done!"
